46b99bc2d827bd9019e494baf41614b4612ab5ce
Cawwot1 <Yang_Me@student.kings.edu.au>
2025-07-22 14:37:03 +1000

    - Debug turned back on (for marking)

--- server/tempCodeRunnerFile.py
+++ server/tempCodeRunnerFile.py
@@ -1,14 +1,1 @@
-@app.route('/forgot-password', methods=['POST'])
-def forgot_password():
-    data = request.get_json()
-    username = escape(data.get('forgotUsername'))
-    email = escape(data.get('forgotEmail'))
-
-    if not username or not email:
-        return jsonify({'error': 'Username and email are required'}), 400
-    
-    result = verify_username_with_email(email, username)
-    if result:
-        return make_response(jsonify({"success": True}), 200)
-    else:
-        return jsonify({'error': 'Wrong username or email'}), 400
+debug
